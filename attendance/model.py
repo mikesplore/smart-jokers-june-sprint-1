9ed29b526fea22f7fa7d578ee6a112c803dcfe6b
1303LIZ <mwaranganga@gmail.com>
2025-06-17 13:26:26 +0300

    added model.py

---
+++ attendance/model.py
@@ -0,0 +1,19 @@
+
+from django.db import models
+from django.utils import timezone
+from users.models import User
+
+
+class AttendanceRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='attendance_records')
+    check_in_time = models.DateTimeField(null=True, blank=True)
+    check_out_time = models.DateTimeField(null=True, blank=True)
+    date = models.DateField(default=timezone.now)
+    
+class Meta:
+        unique_together = ['user', 'date']
+        ordering = ['-date', '-check_in_time']
+    
+    
+def __str__(self):
+        return f"{self.user.email} - {self.date}"
